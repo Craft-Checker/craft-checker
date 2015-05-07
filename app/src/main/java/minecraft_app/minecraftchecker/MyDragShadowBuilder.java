b5d1a36c4cc6968783d031e1ac3f8bd94ac2e3c3
mmlevy <gx200df@aol.com>
2015-05-06 19:17:13 -0700

    Drag effect now drags ghost image of original image being dragged. Fixed table squares to use background color instead of default white box image. Fixed Clear button overlap issue.

--- app/src/main/java/minecraft_app/minecraftchecker/MyDragShadowBuilder.java
+++ app/src/main/java/minecraft_app/minecraftchecker/MyDragShadowBuilder.java
@@ -3,9 +3,13 @@ package minecraft_app.minecraftchecker;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
+
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import la_hacks.minecraftchecker.R;
 
 /**
  * Created by Marion on 5/2/15.
@@ -13,20 +17,33 @@ import android.view.View;
 public class MyDragShadowBuilder extends View.DragShadowBuilder {
     private static Drawable shadow;
 
+    private ImageView imageView;
+
     public MyDragShadowBuilder(View v) {
         super(v);
-        shadow = new ColorDrawable(Color.LTGRAY);
+        imageView = (ImageView) v;
+        //shadow = new ColorDrawable(Color.LTGRAY);
+
+        //Drawable d = v.getContext().getResources().getDrawable(res);
+
+        Drawable clone = imageView.getDrawable().getConstantState().newDrawable();
+        shadow = clone;
+
+//        ImageView image = (ImageView)findViewById(R.id.image);
+//        image.setImageDrawable(d);
     }
     public void onProvideShadowMetrics(Point size, Point touch) {
         int width, height;
-        // sets shadow width and height to half of the original view
-        width = getView().getWidth() / 2;
-        height = getView().getHeight() / 2;
+        // sets shadow width and height to size of the original view
+        Log.i("TEST", "called");
+        width = getView().getWidth();
+        height = getView().getHeight();
         shadow.setBounds(0, 0, width, height);
         size.set(width, height);
         // sets touch point's position to be in the middle of the drag shadow
         touch.set(width / 2, height / 2);
     }
+
     public void onDrawShadow(Canvas canvas) {
         shadow.draw(canvas);
     }
